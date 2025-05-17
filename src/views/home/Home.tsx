1538879ecedd3fec453aafb91d04de3e07067e6b
Rayner3103 <80162238+Rayner3103@users.noreply.github.com>
2025-05-18 00:20:30 +0800

    Created react app

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+    console.log('Button clicked!');
+    fetch('http://localhost:5000/test/')
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            navigate('/other', { state: { data } });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is a simple home page component.</p>
+            <button onClick={handleButtonClick}>Test</button>
+        </div>
+    )
+}
+
+export default Home;
