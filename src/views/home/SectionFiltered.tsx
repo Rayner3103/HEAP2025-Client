6439541853d98bc68d569548339802ccc2671f0f
bljr07 <beyblade1704@gmail.com>
2025-07-12 11:42:28 +0800

    FIX: Removed debug log

--- src/views/home/SectionFiltered.tsx
+++ src/views/home/SectionFiltered.tsx
@@ -4,7 +4,6 @@ import * as EventInterface from "@/interface/event";
 const SectionHackathons = ({events, filters}: {events: EventInterface.Event[], filters: string[]}) => {
   // Filter for hackathon events
   const filteredEvents = events.filter(event => filters.every(filter => event.tags.includes(filter)));
-  console.log(filteredEvents);
   return (
     <section className="w-full">
       <h3 className="text-2xl font-semibold mb-4">Filtered Opportunities</h3>
